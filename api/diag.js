f0254d684b619760751b3154a08a0816d6dd2ba3
emerilansel-jpg <emerilansel@gmail.com>
2026-04-18 22:54:56 +0700

    Update diag.js

--- api/diag.js
+++ api/diag.js
@@ -1,4 +1,3 @@
-// Diag v5 - find _wantsCount scoping issue
 var fs = require('fs');
 var path = require('path');
 module.exports = function(req, res) {
@@ -6,23 +5,18 @@ module.exports = function(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   var src = fs.readFileSync(path.join(__dirname, 'chat.js'), 'utf8');
   var lines = src.split('\n');
-  var report = { total: lines.length, wantsCount: [], tryBlocks: [], catchBlocks: [], edgeGuard: [], braces305_340: [] };
-  for (var i = 0; i < lines.length; i++) {
-    var l = lines[i];
-    if (l.includes('_wantsCount')) report.wantsCount.push(i+1);
-  }
-  // Get brace depth changes around lines 300-345
-  for (var i = 299; i < 345 && i < lines.length; i++) {
+  var depth = 0;
+  var report = [];
+  for (var i = 229; i < 315 && i < lines.length; i++) {
     var l = lines[i];
     var opens = (l.match(/\{/g) || []).length;
     var closes = (l.match(/\}/g) || []).length;
-    var indent = l.match(/^(\s*)/)[1].length;
-    var hasTry = l.includes('try');
-    var hasCatch = l.includes('catch');
-    var hasIf = l.includes('if (') || l.includes('if(');
-    var hasReturn = l.includes('return ');
-    var hasVar = l.includes('var ') || l.includes('let ') || l.includes('const ');
-    report.braces305_340.push({n:i+1, ind:indent, o:opens, c:closes, try:hasTry||undefined, catch:hasCatch||undefined, if:hasIf||undefined, ret:hasReturn||undefined, v:hasVar||undefined});
+    depth += opens - closes;
+    var info = {n:i+1, d:depth};
+    if (l.includes('_wantsCount')) info.wc = l.includes('let ')?'let':l.includes('var ')?'var':l.includes('const ')?'const':'use';
+    if (l.includes('try')) info.t = 1;
+    if (l.includes('catch')) info.ct = 1;
+    if (opens > 0 || closes > 0 || info.wc || info.t || info.ct) report.push(info);
   }
   res.status(200).json(report);
 };
