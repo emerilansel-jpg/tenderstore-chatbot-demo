03dd6f5efbf64ada2b5021ec1a7d2ef112b125d4
emerilansel-jpg <emerilansel@gmail.com>
2026-04-21 16:55:40 +0700

    Update parse-tenders.js

--- api/parse-tenders.js
+++ api/parse-tenders.js
@@ -86,7 +86,7 @@ ATURAN:
       const _today = new Date();
       const _months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
       const _tglPengumuman = _today.getDate().toString().padStart(2,'0') + '-' + _months[_today.getMonth()] + '-' + _today.getFullYear();
-      data.forEach(function(item) { if(!item.tanggal_pengumuman) item.tanggal_pengumuman = _tglPengumuman; });
+      data.forEach(function(item) { if(!item.tanggal_pengumuman) item.tanggal_pengumuman = _tglPengumuman; if(item.kategori) item.kategori = item.kategori.toUpperCase(); });
       if (Array.isArray(data) && data.length > 0) {
         return res.status(200).json({ data: data });
       }
